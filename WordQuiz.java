52a0c9157419f2d46ef06d404bab9a3d47134fc2
adamthehackerx <57100052+adamthehackerx@users.noreply.github.com>
2019-12-05 12:03:21 +0100

    Update WordQuiz.java

--- WordQuiz.java
+++ WordQuiz.java
@@ -1,98 +1,114 @@
 package laboration5;
-import java.io.*;
 import java.util.*;
-
-public class Dictionary {
+import java.io.*;
+public class WordQuiz {
 	
-	Map<Word,Set<Word>> map = new HashMap<Word,Set<Word>>();
-	
-	public void add(Word t, Word m) {
-		if(map.containsKey(t)) {
-			Set<Word> nyttM = map.get(t);
-			nyttM.add(m);
-		}
-		else {
-			Set<Word> newSet = new HashSet<>();
-			newSet.add(m);
-			map.put(t, newSet);
-		}
-		
+	Dictionary dictionary;
+	public WordQuiz(Dictionary dictionary) {
+		this.dictionary = dictionary;
 	}
 	
-	public void add(String keys, String values) {
-		add(new Word(keys), new Word(values));
-	}
-	
-	
-	 // Returnerar en icke-null mängd med ordlistans alla termer.
-	 
-	public Set<Word> terms() {
-		return map.keySet();
-	}
-	
-	
-	 // Slår upp och returnerar en mängd av betydelser till t,
-	 // eller null om t inte finns i ordlistan
-	 
-	
-	public Set<Word> lookup(Word t) {
-		return map.get(t);
-	}
-	 
-	 // Skapar och returnerar en ny ordlista på det motsatta språket,
-	 // dvs, alla betydelser blir termer och alla termer blir betydelser.
-	 // T.ex en svensk-engelsk ordlist blir efter invertering engels-svensk.
-	
-	public Dictionary inverse() {
-		Dictionary inverseDictionary = new Dictionary();
-		
-		for(Word key : map.keySet()) {
-			for (Word value : lookup(key)) {
-			inverseDictionary.add(value, key);
+	public void runQuiz() {
+		Set<Word> keys = dictionary.terms();
+		Scanner scan = new Scanner(System.in);
+		List<Word> list = new ArrayList<>(keys);
+		List<Word> rattList = new ArrayList<>();
+		int ratt = 0;
+		int fel = 0;
+		int wordCount = 1;
+		while(!(list.isEmpty())) {
+			
+			for(Word key : list) {
+				System.out.println("Word " + wordCount + " is: " + key);
+				Word value = new Word(scan.nextLine());
+				
+				if(dictionary.lookup(key).contains(value)) {
+					System.out.println("Rätt!");
+					ratt++;
+					rattList.add(key);
+				} else {
+					System.out.println("Fel!");
+					fel++;
+				}	
+				wordCount++;
+			}
+			for(Word key : rattList) {
+				list.remove(key);
+			}
+			if (list.size() != 0) {
+			System.out.println("Du hade " + fel + " fel, kör igen!");
+			wordCount = 1;
 			}
 		}
-		return inverseDictionary;
+		
+		System.out.println("Du hade " + ratt + " rätt och " + fel + " Fel");
 		
 	}
 	
-	 
-	//  Läser in orden från den givna strömmen och lägger dessa i ordlistan.
-	 
-	
-	public void load(InputStream is) {
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-	        while (reader.ready()) {
-	            String line = reader.readLine();
-	            String[] words = line.split(",");
-	            add(words[0],words[1]);
-	           
-	        }
+	private static int showMenu() {
+		System.out.println("1. Add Word");
+		System.out.println("2. Play Swedish to English");
+		System.out.println("3. Play English to Swedish");
+		System.out.println("4. Quit");
+		
+		Scanner input = new Scanner(System.in);
+		int choice = input.nextInt();
+		return choice;
 
-	        
-	    }catch (FileNotFoundException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	    } catch (IOException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	    }
 	}
 	
-	
-	 // Lagrar ordlistan på den givna strömmen.
-	 
-	
-	public void save(OutputStream os) throws IOException {
-		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os);
-		for(Word key : map.keySet()) {
-				for(Word value : map.get(key)) {
-					outputStreamWriter.write(key + "," + value + "\n");
+	public void menu() throws IOException, FileNotFoundException {
+		System.out.println("Welcome To My Wordquiz");
+		System.out.println("======================");
+		System.out.println("Select one of the following:");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		InputStream ordFil = new FileInputStream ("/Users/adam/eclipse-workspace/laboration5/src/laboration5/ord.txt");
+		dictionary.load(ordFil);
+		
+		int a = 0;
+		int i = 0;
+		
+		while (a != 4) {
+			a = showMenu();
+			switch(a){
+				case 1:
+					System.out.println("Write Svenska,Engelska");
+					OutputStream spara = new FileOutputStream ("/Users/adam/eclipse-workspace/laboration5/src/laboration5/ord.txt");
+					String line = scan.nextLine();
+					String[] word = line.split(",");
+					this.dictionary.add(word[0], word[1]);
+					dictionary.save(spara);
+					break;
 					
-				}
+				case 2:
+					runQuiz();				
+					break;
+					
+				case 3:
+					dictionary = dictionary.inverse();
+					runQuiz();
+					dictionary = dictionary.inverse();	
+					break;
+					
+				case 4:
+					System.out.println("Goodbye!");
+					System.exit(0);
+					break;
+					
+				default:
+					System.out.println("Skriv in ett nummer mellan 1-4 din sopa");
+					break;
+			}
 		}
-
-		outputStreamWriter.close();
 	}
-	
-	
+
+	public static void main(String[] args) throws IOException {
+// Skapa en tom ordlista på ngt sätt och fyll den med ord. Dictionary
+		
+		Dictionary sweng = new Dictionary();
+		WordQuiz wq = new WordQuiz(sweng);
+		wq.menu();  
+	}
 }
